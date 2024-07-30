86ac2b496f625a3eec740b9e9eb2b20f35af5174
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-07-31 00:25:08 +0530

    Validating user data

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { create } = require("../controllers/user");
+const { userValidCheck, validate } = require("../middlewares/validator");
 const router = express.Router();
 
 //Post -> safe, get-> not safe.  Always think from front-end.
-router.post("/create", create); // Here we are getting info from front-end. (POST for frontend)
+router.post("/create", userValidCheck, validate, create); // Here we are getting info from front-end. (POST for frontend)
 
 module.exports = router;
