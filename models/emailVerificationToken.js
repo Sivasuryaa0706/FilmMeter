40dfcae7d58416f5f7de5ee02902b3d09fbe24b8
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-07-31 22:12:10 +0530

    Email Verification Token Schema

---
+++ models/emailVerificationToken.js
@@ -0,0 +1,39 @@
+//!We use this schema to store tokens using owner id.
+// verificationToken:{
+//     owner: _id,
+//     token: otp (in hashed format),
+//     expiryDate: 1hr
+// }
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const emailVerificationTokenSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId, //Stored as ObjectId datatype in db.
+    ref: "User", //In the model/user we are exporting it as "User"
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 3600,
+    default: Date.now(),
+  },
+});
+
+// Hash token beforing saving.
+emailVerificationTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    const hash = await bcrypt.hash(this.token, 10);
+    this.token = hash;
+  }
+  next();
+});
+
+module.exports = mongoose.model(
+  "EmailVerficationToken",
+  emailVerificationTokenSchema
+);
