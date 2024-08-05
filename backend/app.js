dbfa0a8cbc8350b4a67d33a1650ee71629a6e2c7
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-05 17:59:38 +0530

    Async Await Error Handling

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+require("express-async-errors");
 require("dotenv").config({ path: "../.env" });
 require("./db");
 const userRoute = require("./routes/user");
+const { errorHandler } = require("./middlewares/error");
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/user", userRoute);
+
+//Async Await Error Handling using express-async-errors
+//We use this so that no need of try catch block for all async await functions
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Listening on port 8000");
