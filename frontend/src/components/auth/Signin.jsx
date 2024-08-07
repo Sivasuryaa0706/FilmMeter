eb94f1f0c10ef8061d455e0fd22eb43348b635c2
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-07 22:42:05 +0530

    Refactor Signin Form

--- frontend/src/components/auth/Signin.jsx
+++ frontend/src/components/auth/Signin.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
 
 export default function Signin() {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
-        <form className="bg-secondary rounded p-6 w-99">
-          <h1 className="text-xl text-white font-semibold text-center">
-            Sign in
-          </h1>
-          <div className="flex flex-col-reverse">
-            <input
-              type="text"
-              id="email"
-              className="bg-transparent rounded border-2 border-dark-subtle
-              text-lg w-full outline-none focus:border-white p-1 text-white peer transition"
-              placeholder="john@gmail.com"
-            />
-            <label
-              className="font-semibold text-dark-subtle peer-focus:text-white self-start"
-              htmlFor="email"
-            >
-              Email
-            </label>
-          </div>
+        <form className="bg-secondary rounded p-6 space-y-6">
+          <Title>Sign In</Title>
+          <FormInput
+            label="Email"
+            placeholder="john@email.com"
+            name="email"
+          ></FormInput>
+          <FormInput
+            label="Password"
+            placeholder="********"
+            name="password"
+          ></FormInput>
         </form>
       </Container>
     </div>
---
+++ frontend/src/components/form/FormInput.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function FormInput({ name, placeholder, label, ...rest }) {
+  return (
+    <div className="flex flex-col-reverse">
+      <input
+        id={name}
+        name={name}
+        className="bg-transparent rounded border-2 border-dark-subtle
+              text-lg w-full outline-none focus:border-white p-1 text-white peer transition"
+        placeholder={placeholder}
+        {...rest}
+      />
+      <label
+        className="font-semibold text-dark-subtle peer-focus:text-white self-start"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
