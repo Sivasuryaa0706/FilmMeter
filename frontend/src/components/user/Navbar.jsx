3cf78bf87bb1c4a6eb89767a3529edee30b686ae
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-10 22:27:05 +0530

    Fixing navigation home and home route

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { IoIosSunny } from "react-icons/io";
 import Container from "../Container";
 
@@ -7,12 +8,14 @@ export default function Navbar() {
     <div className="bg-secondary shadow-sm shadow-gray-500">
       <Container>
         <div className="flex justify-between items-center">
-          <img
-            src="./logo-dark.png"
-            height="100px"
-            width="100px"
-            className="h-18 w-35"
-          />
+          <Link to="/">
+            <img
+              src="./logo-dark.png"
+              height="100px"
+              width="100px"
+              className="h-18 w-35"
+            />
+          </Link>
           <ul className="flex items-center space-x-4">
             <li>
               <button className="bg-dark-subtle p-1 rounded">
@@ -28,7 +31,12 @@ export default function Navbar() {
                 placeholder="Search..."
               />
             </li>
-            <li className="text-white font-semibold text-lg">Login</li>
+            <Link
+              className="text-white font-semibold text-lg"
+              to="/auth/signin"
+            >
+              Login
+            </Link>
           </ul>
         </div>
       </Container>
