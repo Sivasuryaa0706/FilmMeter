3db5a2113928db45e3df1ca2a47c2f1959b84a32
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-10 22:19:42 +0530

    Confirm Password UI & Base root

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
 
 export default function ConfirmPassword() {
-  return <div>ConfirmPassword</div>;
+  return (
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-96 space-y-6">
+          <Title>Enter New Password</Title>
+          <FormInput
+            label="New Password"
+            placeholder="********"
+            name="password"
+            type="password"
+          ></FormInput>
+          <FormInput
+            label="Confirm Password"
+            placeholder="********"
+            name="confirmPassword"
+            type="password"
+          ></FormInput>
+          <Submit value="Confirm Password" />
+        </form>
+      </Container>
+    </div>
+  );
 }
