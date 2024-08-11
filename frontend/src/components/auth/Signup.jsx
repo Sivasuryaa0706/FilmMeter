de0f28caeb75b2434fe1a962238e027b8ff365a4
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-11 17:14:36 +0530

    SignUp state (UserInfo)

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
@@ -8,21 +8,49 @@ import { commonModalClasses } from "../../utils/Theme";
 import FormContainer from "../form/FormContainer";
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = userInfo;
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses}>
+        <form onSubmit={handleSubmit} className={commonModalClasses}>
           <Title>Sign Up</Title>
-          <FormInput label="Name" placeholder="John" name="name"></FormInput>
           <FormInput
+            value={name}
+            onChange={handleChange}
+            label="Name"
+            placeholder="John"
+            name="name"
+          ></FormInput>
+          <FormInput
+            value={email}
+            onChange={handleChange}
             label="Email"
             placeholder="john@email.com"
             name="email"
           ></FormInput>
           <FormInput
+            value={password}
+            onChange={handleChange}
             label="Password"
             placeholder="********"
             name="password"
+            type="password"
           ></FormInput>
           <Submit value="Sign up" />
           <div className="flex justify-between">
