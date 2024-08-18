115a70aa3be7aa1b2119b7679d15c2a9b6d2d8d8
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-18 21:12:47 +0530

    Handling Invalid Token

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
@@ -7,14 +7,47 @@ import Submit from "../form/Submit";
 import FormContainer from "../form/FormContainer";
 import { commonModalClasses } from "../../utils/Theme";
 import { ImSpinner3 } from "react-icons/im";
+import { verifyPasswordResetToken } from "../../api/auth";
+import { useNotification } from "../../hooks";
 
 export default function ConfirmPassword() {
   const [isVerifying, setIsVerifying] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
 
-  //isTokenValid, isVerifying, !isValid
+  const { updateNotification } = useNotification();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    isValidToken();
+  }, []);
+
+  const isValidToken = async () => {
+    const { error, valid } = await verifyPasswordResetToken(token, id);
+    setIsVerifying(false);
+    if (error) return updateNotification("error", error);
+
+    if (!valid) {
+      setIsValid(false);
+      return navigate("/auth/reset-password", { replace: true });
+    }
+    setIsValid(true);
+  };
+
+  if (!isValid)
+    return (
+      <FormContainer>
+        <Container>
+          <h1 className="dark:text-white text-primary text-2xl font-semibold">
+            Sorry the token is Invalid!
+          </h1>
+        </Container>
+      </FormContainer>
+    );
+
   if (isVerifying)
     return (
       <FormContainer>
