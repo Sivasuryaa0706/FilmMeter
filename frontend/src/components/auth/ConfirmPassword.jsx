ce71969f9bbce5dd1e0490050c418ee04b147bb5
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-18 20:50:53 +0530

    Rendered Reset Password isVerifying Indicator

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Container from "../Container";
 import Title from "../form/Title";
@@ -6,13 +6,28 @@ import FormInput from "../form/FormInput";
 import Submit from "../form/Submit";
 import FormContainer from "../form/FormContainer";
 import { commonModalClasses } from "../../utils/Theme";
+import { ImSpinner3 } from "react-icons/im";
 
 export default function ConfirmPassword() {
+  const [isVerifying, setIsVerifying] = useState(true);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
 
   //isTokenValid, isVerifying, !isValid
+  if (isVerifying)
+    return (
+      <FormContainer>
+        <Container>
+          <div className="flex space-x-2 items-center">
+            <h1 className="dark:text-white text-primary text-2xl font-semibold">
+              Please wait while we are verifying your token!
+            </h1>
+            <ImSpinner3 className="animate-spin text-2xl dark:text-white text-primary" />
+          </div>
+        </Container>
+      </FormContainer>
+    );
 
   return (
     <FormContainer>
