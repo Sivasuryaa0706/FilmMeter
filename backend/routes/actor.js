bd5f9f7cdb4ba088af7866b8aaf8bd4ada2bcade
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-19 16:13:27 +0530

    Middleware to validate actor

--- backend/routes/actor.js
+++ backend/routes/actor.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { createActor } = require("../controllers/actor");
 const { uploadImage } = require("../middlewares/multer");
+const { actorInfoValidator } = require("../middlewares/validator");
 const router = express.Router();
 
-router.post("/create", uploadImage.single("avatar"), createActor);
+router.post(
+  "/create",
+  uploadImage.single("avatar"),
+  actorInfoValidator,
+  createActor
+);
 module.exports = router;
