25d055652a530f001e6bfbb9526fcc27fff3a53d
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-25 20:58:12 +0530

    Created Movie Schema

---
+++ backend/utils/genres.js
@@ -0,0 +1,25 @@
+const genres = [
+  "Action",
+  "Adventure",
+  "Animation",
+  "Biography",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Family",
+  "Fantasy",
+  "Film-Noir",
+  "History",
+  "Horror",
+  "Musical",
+  "Mystery",
+  "Romance",
+  "Sci-Fi",
+  "Sport",
+  "Thriller",
+  "War",
+  "Western",
+];
+
+module.exports = genres;
