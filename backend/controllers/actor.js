e981ffec9c86fd96f5239ac3897b8d668bc4fb40
Sivasuryaa <sivasuryaa2004@gmail.com>
2024-08-25 09:07:45 +0530

    Refactoring Actor Controller

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -1,14 +1,11 @@
 const { isValidObjectId } = require("mongoose");
 const Actor = require("../models/actor");
-const { sendError } = require("../utils/helper");
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-  secure: true,
-});
+const {
+  sendError,
+  uploadImageToCloud,
+  formatActor,
+} = require("../utils/helper");
+const cloudinary = require("../cloud");
 
 exports.createActor = async (req, res) => {
   const { name, about, gender } = req.body;
@@ -17,21 +14,12 @@ exports.createActor = async (req, res) => {
   const newActor = new Actor({ name, about, gender });
 
   if (file) {
-    const { secure_url, public_id } = await cloudinary.uploader.upload(
-      file.path,
-      { gravity: "face", height: 500, width: 500, crop: "thumb" }
-    );
-    newActor.avatar = { url: secure_url, public_id };
+    const { url, public_id } = await uploadImageToCloud(file.path);
+    newActor.avatar = { url, public_id };
   }
   await newActor.save();
 
-  res.status(201).json({
-    id: newActor._id,
-    name,
-    about,
-    gender,
-    avatar: newActor.avatar?.url,
-  });
+  res.status(201).json(formatActor(actor));
 };
 
 exports.updateActor = async (req, res) => {
@@ -55,11 +43,13 @@ exports.updateActor = async (req, res) => {
 
   //upload new avatar if there is one!
   if (file) {
-    const { secure_url, public_id } = await cloudinary.uploader.upload(
-      file.path,
-      { gravity: "face", height: 500, width: 500, crop: "thumb" }
-    );
-    actor.avatar = { url: secure_url, public_id };
+    const { url, public_id } = await cloudinary.uploader.upload(file.path, {
+      gravity: "face",
+      height: 500,
+      width: 500,
+      crop: "thumb",
+    });
+    actor.avatar = { url, public_id };
   }
 
   actor.name = name;
@@ -67,13 +57,7 @@ exports.updateActor = async (req, res) => {
   actor.gender = gender;
   await actor.save();
 
-  res.status(201).json({
-    id: actor._id,
-    name,
-    about,
-    gender,
-    avatar: actor.avatar?.url,
-  });
+  res.status(201).json(formatActor(actor));
 };
 
 exports.deleteActor = async (req, res) => {
@@ -101,12 +85,15 @@ exports.searchActor = async (req, res) => {
   const { query } = req;
   const result = await Actor.find({ $text: { $search: `"${query.name}"` } });
 
-  res.json(result);
+  const actors = result.map((actor) => formatActor(actor));
+  res.json(actors);
 };
 
 exports.getLatestActors = async (req, res) => {
   const result = await Actor.find().sort({ createdAt: "-1" }).limit(12);
-  res.json(result);
+
+  const actors = result.map((actor) => formatActor(actor));
+  res.json(actors);
 };
 
 exports.getSingleActor = async (req, res) => {
@@ -116,5 +103,5 @@ exports.getSingleActor = async (req, res) => {
   const actor = await Actor.findById(id);
   if (!actor) return sendError(res, "Invalid request, Actor not found!", 404);
 
-  res.json(actor);
+  res.json(formatActor(actor));
 };
--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,4 +1,6 @@
 const crypto = require("crypto");
+const cloudinary = require("../cloud");
+
 exports.sendError = (res, error, statusCode = 401) => {
   res.status(statusCode).json({ error });
 };
@@ -16,4 +18,22 @@ exports.generateRandomBytes = () => {
 
 exports.handleNotFound = (req, res) => {
   this.sendError(res, "Not Found", 404);
+};
+
+exports.uploadImageToCloud = async (filePath) => {
+  const { secure_url: url, public_id } = await cloudinary.uploader.upload(
+    filePath,
+    {
+      gravity: "face",
+      height: 500,
+      width: 500,
+      crop: "thumb",
+    }
+  );
+  return { url, public_id };
+};
+
+exports.formatActor = (actor) => {
+  const { name, gender, about, _id, avatar } = actor;
+  return { id: _id, name, about, gender, avatar: actor.avatar?.url };
 };
